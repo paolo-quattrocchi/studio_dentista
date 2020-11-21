8319892bd3b251d3bceebd0901fffd672f6c9f5f
valeria vallese <you@example.com>
2020-11-21 16:37:30 +0100

    modifiche completate in servizi con funzioni

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,13 +1,12 @@
 <x-layout>
-    <div class="container my-5 py-5 bg-dark">
+    <div class="container my-5 py-5 ">
         <div class="col-12">
             <h2>Ecco i nostri servizi!</h2>
             <div class="row">
                 @foreach ($services as $service)
                   <div class="col-12 col-md-4 my-3">
                     <x-card
-                    name="{{$service['name']}}"
-                    surname="{{$service['surname']}}"
+                    servizio="{{$service['servizio']}}"
                     img="{{$service['img']}}"
                     description="{{$service['description']}}"
                     />          
