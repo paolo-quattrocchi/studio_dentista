d0024fd8150896800836a29e3a5b204b60ed88e8
flavio faraone <flaviofaraon@hotmail.it>
2020-11-22 12:47:22 +0100

    creata la pagina contacts con form per i dati

--- public/js/app.js
+++ public/js/app.js
@@ -515,20 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
 __webpack_require__(/*! C:\Users\Windows\wa\laravel\p_r_v_f_d_dentista\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Users\Windows\wa\laravel\p_r_v_f_d_dentista\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Users\Utente\wa\laravel\p_r_v_f_d_dentista\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Utente\wa\laravel\p_r_v_f_d_dentista\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> 8319892bd3b251d3bceebd0901fffd672f6c9f5f
-=======
->>>>>>> f126bd1f9f04c19beb5e2b06be9507eab6f24e8f
-__webpack_require__(/*! C:\Users\Utente Locale\wa\laravel\p_r_v_f_d_dentista\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Utente Locale\wa\laravel\p_r_v_f_d_dentista\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
